b5be4b5d9a7590eb72dac141f963e2e34b7ca5d7
Doug Watson <dwwatso@yahoo.com>
2017-11-16 06:47:14 +0000

    2 of 4 cukes passing

--- app.rb
+++ app.rb
@@ -40,6 +40,12 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      result = @game.guess(letter)
+      set flash[:message] = "You have already used that letter." unless result
+    rescue
+      set flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
